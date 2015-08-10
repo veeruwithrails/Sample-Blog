b2356251dd65f9597966e748f04fc6d4f492856d
it197 <it197@it197s-Mac-mini.local>
2015-08-10 00:44:30 -0700

    post module added

--- sample-blog/db/migrate/20150810063010_create_posts.rb
+++ sample-blog/db/migrate/20150810063010_create_posts.rb
@@ -1,10 +1,13 @@
 class CreatePosts < ActiveRecord::Migration
-  def change
+  def up
     create_table :posts do |t|
       t.string :title
       t.text :description
 
       t.timestamps null: false
     end
+  end
+  def down
+  	remove_table :posts
   end
 end
